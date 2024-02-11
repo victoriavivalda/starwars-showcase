87aed300acd345a938cf8c362b4404dc08d4a3c5
Victoria <vicky_864@hotmail.com>
2024-02-11 17:35:01 -0300

    Add no data message

--- app/ui/tableMostViewed.tsx
+++ app/ui/tableMostViewed.tsx
@@ -1,4 +1,8 @@
-import { TableStructureMostViewed } from '@/app/lib/definitions';
+import {
+  MostVisitedItems,
+  TableHeaders,
+  TableStructureMostViewed,
+} from '@/app/lib/definitions';
 import Link from 'next/link';
 
 export default function TableMostViewed({
@@ -9,47 +13,78 @@ export default function TableMostViewed({
   return (
     <section>
       <h2 className='font-bold camelcase text-xs sm:text-lg'>{title}</h2>
-
       <div className='flex'>
         <table className='table-fixed w-full text-sm border-separate border-spacing-y-2'>
           <thead className=''>
-            <tr className='px-4 py-3 text-gray-200 bg-gray-900 first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg'>
-              {headers.map(({ label, field }) => (
-                <th key={field}>{label}</th>
-              ))}
-            </tr>
+            <TableHeadRow headers={headers} />
           </thead>
           <tbody>
-            {data?.map((item, i) => {
-              return (
-                <tr key={i}>
-                  {Object.entries(item)
-                    .filter(([key]) =>
-                      headers.map((i) => i.field).includes(key)
-                    )
-                    .map(([key, value]) => {
-                      return (
-                        <td
-                          key={key}
-                          className='px-4 py-3 text-gray-900 bg-gray-200 first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg'>
-                          {key === 'name' ? (
-                            <Link
-                              className='cursor-pointer'
-                              href={`/${item.resource}/${item.id}/`}>
-                              {value}
-                            </Link>
-                          ) : (
-                            value
-                          )}
-                        </td>
-                      );
-                    })}
-                </tr>
-              );
-            })}
+            {data && data.length ? (
+              data.map((item, i) => (
+                <TableBodyRow
+                  key={i}
+                  item={item}
+                  headers={headers}
+                />
+              ))
+            ) : (
+              <TableBodyRowWithoutData />
+            )}
           </tbody>
         </table>
       </div>
     </section>
+  );
+}
+
+function TableHeadRow({ headers }: { headers: TableHeaders[] }) {
+  return (
+    <tr className='px-4 py-3 text-gray-200 bg-gray-900 first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg'>
+      {headers.map(({ label, field }) => (
+        <th key={field}>{label}</th>
+      ))}
+    </tr>
+  );
+}
+
+function TableBodyRow({
+  item,
+  headers,
+}: {
+  item: MostVisitedItems;
+  headers: TableHeaders[];
+}) {
+  return (
+    <tr>
+      {Object.entries(item)
+        .filter(([key]) => headers.map((i) => i.field).includes(key))
+        .map(([key, value]) => {
+          return (
+            <td
+              key={key}
+              className='px-4 py-3 text-gray-900 bg-gray-200 first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg'>
+              {key === 'name' ? (
+                <Link
+                  className='cursor-pointer'
+                  href={`/${item.resource}/${item.id}/`}>
+                  {value}
+                </Link>
+              ) : (
+                value
+              )}
+            </td>
+          );
+        })}
+    </tr>
+  );
+}
+
+function TableBodyRowWithoutData() {
+  return (
+    <tr>
+      <td className='px-4 py-3 text-gray-900 bg-gray-200 font-medium text-center'>
+        Nothing to display yet
+      </td>
+    </tr>
   );
 }
